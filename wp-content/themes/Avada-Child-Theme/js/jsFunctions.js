c72b878a851b5a891a47b037fb7d5f58ec4bcb44
axeljohansson1988 <axeljohansson88@gmail.com>
2016-09-21 14:33:20 +0200

    menu bug

--- wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
+++ wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
@@ -2,27 +2,6 @@
 var mobileBreakPoint = 1024;
 var screenWidth = jQuery(window).width();
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    //Set the margin on document ready to prevent ugly loading on mobile devices
-    jQuery('#content').css('margin-top', '83px');
-    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
-        jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
-    }
-    setWhyAnchor();
-});
-//On window resize
-jQuery(window).resize(function () {
-    screenWidth = jQuery(window).width();
-    setWhyAnchor();
-});
-//On scroll
-jQuery(window).scroll(function () {
-    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
-        jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
-    }
-});
-//Sets the whysapigo anchor in right place depending on screnn width
 var setWhyAnchor = function () {
     if (screenWidth <= mobileBreakPoint) {
         jQuery('#whysapigo').addClass('to-bottom');
@@ -32,3 +11,42 @@ var setWhyAnchor = function () {
         jQuery('#whysapigo').insertBefore('#why-sapigo-placeholder');
     }
 };
+document.addEventListener('DOMContentLoaded', function () {
+    //Set the margin on document ready to prevent ugly loading on mobile devices
+    //    j    //    jQuery('#content').css('padding-top', '83px');
+    //    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
+    //        jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
+    //    }
+    setWhyAnchor();
+    //    setWelcomeImgPadding();
+});
+//On window resize
+jQuery(window).resize(function () {
+    screenWidth = jQuery(window).width();
+    setWhyAnchor();
+    //    setWelcomeImgPadding();
+
+});
+//On scroll
+//jQuery(window).scroll(function () {
+//    //    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
+//    //    jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
+//    //    }
+//});
+//Sets the whysapigo anchor in right place depending on screnn width
+
+//var setWelcomeImgPadding = function () {
+//    if (screenWidth <= mobileBreakPoint) {
+//        //        jQuery('#welcome-img h1').css('margin-top', '83px');
+//        //        jQuery('#content').css('margin-top', '0');
+//
+//        //        jQuery('.post-content').css('margin-top', '83px');
+//
+//    } else {
+//        //        jQuery('#content').css('margin-top', '83px');
+//        //        jQuery('#welcome-img h1').css('margin-top', '0');
+//
+//        //        jQuery('.post-content').css('margin-top', '83px');
+//
+//    }
+//}
