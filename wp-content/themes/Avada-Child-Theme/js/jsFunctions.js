b13c1e00ec2e8b46b63188500acd3ffb5e0a7664
axeljohansson1988 <axeljohansson88@gmail.com>
2016-09-21 10:48:08 +0200

    mobile margin menu bug

--- wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
+++ wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
@@ -4,14 +4,23 @@ var screenWidth = jQuery(window).width();
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
+    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
+        jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
+    }
     setWhyAnchor();
 });
-
+//On window resize
 jQuery(window).resize(function () {
     screenWidth = jQuery(window).width();
     setWhyAnchor();
 });
+//On scroll
+jQuery(window).scroll(function () {
+    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
+        jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
+    }
+});
+//Sets the whysapigo anchor in right place depending on screnn width
 var setWhyAnchor = function () {
     if (screenWidth <= mobileBreakPoint) {
         jQuery('#whysapigo').addClass('to-bottom');
