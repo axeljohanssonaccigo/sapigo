b97a0de89307006a008a16d336c41d86d5ab7749
axeljohansson1988 <axeljohansson88@gmail.com>
2016-09-21 15:36:42 +0200

    added meta tag to remove auto format of phone number in edge

--- wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
+++ wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
@@ -12,41 +12,10 @@ var setWhyAnchor = function () {
     }
 };
 document.addEventListener('DOMContentLoaded', function () {
-    //Set the margin on document ready to prevent ugly loading on mobile devices
-    //    j    //    jQuery('#content').css('padding-top', '83px');
-    //    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
-    //        jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
-    //    }
     setWhyAnchor();
-    //    setWelcomeImgPadding();
 });
 //On window resize
 jQuery(window).resize(function () {
     screenWidth = jQuery(window).width();
     setWhyAnchor();
-    //    setWelcomeImgPadding();
-
 });
-//On scroll
-//jQuery(window).scroll(function () {
-//    //    if (!jQuery('.fusion-header-wrapper').hasClass('fusion-is-sticky')) {
-//    //    jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
-//    //    }
-//});
-//Sets the whysapigo anchor in right place depending on screnn width
-
-//var setWelcomeImgPadding = function () {
-//    if (screenWidth <= mobileBreakPoint) {
-//        //        jQuery('#welcome-img h1').css('margin-top', '83px');
-//        //        jQuery('#content').css('margin-top', '0');
-//
-//        //        jQuery('.post-content').css('margin-top', '83px');
-//
-//    } else {
-//        //        jQuery('#content').css('margin-top', '83px');
-//        //        jQuery('#welcome-img h1').css('margin-top', '0');
-//
-//        //        jQuery('.post-content').css('margin-top', '83px');
-//
-//    }
-//}
