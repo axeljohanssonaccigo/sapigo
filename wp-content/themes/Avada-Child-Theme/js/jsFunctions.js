205f9e4efc80d3c253f5f93783db7d772779f4dd
axeljohansson1988 <axeljohansson88@gmail.com>
2016-09-21 10:24:55 +0200

    menu bug

--- wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
+++ wp-content/themes/Avada-Child-Theme/js/jsFunctions.js
@@ -1,26 +1,23 @@
 'use strict';
 var mobileBreakPoint = 1024;
+var screenWidth = jQuery(window).width();
+
 
 document.addEventListener('DOMContentLoaded', function () {
-    //    jQuery("#main").attr('style', 'background-attachment: fixed !important');
-    //    document.getElementById('main').style.backgroundAttachment = 'fixed !important';
-    //    console.log(document.getElementById('main').style.backgroundAttachment);
     jQuery('.fusion-header-wrapper').addClass('fusion-is-sticky');
-    //If mobile view
-//    jQuery("#whysapigo").addClass("to-bottom");
-//    jQuery("#whysapigo").appendTo("#welcome-img");
-
-
+    setWhyAnchor();
 });
 
 jQuery(window).resize(function () {
-    var screenWidth = jQuery(window).width();
-    if (screenWidth <= mobileBreakPoint) {
-        jQuery("#whysapigo").addClass("to-bottom");
-        jQuery("#whysapigo").appendTo("#welcome-img");
-    } else {
-
-    }
-    console.log();
-    jQuery("#log").append("<div>Handler for .resize() called.</div>");
+    screenWidth = jQuery(window).width();
+    setWhyAnchor();
 });
+var setWhyAnchor = function () {
+    if (screenWidth <= mobileBreakPoint) {
+        jQuery('#whysapigo').addClass('to-bottom');
+        jQuery('#whysapigo').appendTo('#welcome-img');
+    } else {
+        jQuery('#whysapigo').removeClass('to-bottom');
+        jQuery('#whysapigo').insertBefore('#why-sapigo-placeholder');
+    }
+};
